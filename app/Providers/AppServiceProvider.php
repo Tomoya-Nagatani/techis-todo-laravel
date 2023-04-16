02f3d00b66afe916c17b1dbf8d31ab47530b826f
Tomoya-Nagatani <springdurt@gmail.com>
2023-04-16 11:56:49 +0900

    https 設定

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,9 +27,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if (\App::enviroment(['production'])){
-            \URL::forceScheme('https');
-
+        if (App::enviroment(['production'])){
+            URL::forceScheme('https');
         }
     }
 }
